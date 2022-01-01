808eb25fa939f6211cc18035631381b5b962a3b9
Othniel Ussher <othnielussher16@gmail.com>
2022-01-01 15:01:09 +0000

    Code refactor and clean up

---
+++ lib/app.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:kinshasa/shared/exports.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  DatabaseHelper helper = DatabaseHelper.instance;
+  List<Drink> juiceData = [];
+  List<Drink> smoothieData = [];
+  List<Drink> shakeData = [];
+  List<Drink> favoriteDrinkData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    helper.getJuiceData().then((rows) {
+      rows.shuffle();
+      setState(() {
+        rows.forEach((row) {
+          juiceData.add(Drink.fromMap(row));
+        });
+      });
+    });
+
+    helper.getSmoothieData().then((rows) {
+      rows.shuffle();
+      setState(() {
+        rows.forEach((row) {
+          smoothieData.add(Drink.fromMap(row));
+        });
+      });
+    });
+
+    helper.getShakeData().then((rows) {
+      rows.shuffle();
+      setState(() {
+        rows.forEach((row) {
+          shakeData.add(Drink.fromMap(row));
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => FavoritesBloc(favorites: favoriteDrinkData),
+        ),
+      ],
+      child: MyInheritedWidget(
+        juiceList: juiceData,
+        shakeList: shakeData,
+        smoothieList: smoothieData,
+        child: GetMaterialApp(
+          home: BottomTab(),
+          theme: theme(context),
+          debugShowCheckedModeBanner: false,
+        ),
+      ),
+    );
+  }
+}
